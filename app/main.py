36c9e53634830c2423c72af5cfda6e17b90b7212
mpolawar <37649735+mpolawar@users.noreply.github.com>
2021-07-30 15:05:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #test123 pipeline
 @app.route("/")
 def hello():
-    return "Hello it's XLR/XLD/K8 deployment with deploy_to_k8  from community repo"
+    return "Hello it's XLR/XLD/K8 deployment with deploy_to_k8  from community repo final test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
